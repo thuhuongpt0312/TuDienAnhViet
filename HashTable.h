67820f014ad7114bad4a398d8b59ea0eb01ed2f5
thuhuongpt0312 <thuhuongpt0312@gmail.com>
2026-01-11 10:43:00 +0700

    Add files via upload

---
+++ HashTable.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "LinkedList.h"
+#include <string>
+using namespace std;
+
+class HashTable {
+private:
+    static const int TABLE_SIZE = 26;  
+    LinkedList table[TABLE_SIZE];      
+
+    
+    int hashFunction(string word);
+
+public:
+   
+    HashTable();
+
+    ~HashTable();
+
+    bool addWord(string english, string vietnamese);
+
+    string searchWord(string english);
+
+    bool deleteWord(string english);
+
+    bool updateWord(string english, string newVietnamese);
+
+    void displayAll();
+
+    void displayByLetter(char letter);
+
+    int getTotalWords();
+};
+
+#endif 
