28f4102ceab12242774121ba1997a5d11426e8b5
thuhuongpt0312 <thuhuongpt0312@gmail.com>
2026-01-11 10:38:09 +0700

    Add files via upload

---
+++ Word.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#ifndef WORD_H
+#define WORD_H
+
+#include <string>
+using namespace std;
+
+struct Word {
+    string english;      
+    string vietnamese;   
+    Word* next;          
+
+    Word(string eng, string vie) : english(eng), vietnamese(vie), next(nullptr) {}
+};
+
+#endif 
